caf91cec837945d9fe10a27123c819cde299589a
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-05-21 12:53:13 +0400

    Add PHPUnit configuration and initial tests
    - Updated .gitignore to include/exclude phpunit.xml appropriately.
    - Modified composer.json:
      - Added scripts for running PHPUnit tests (`test`, `test-unit`, `test-feature`).
    - Created phpunit.xml configuration file:
      - Defined two test suites: Unit and Feature.
    - Added sample test files:
      - `tests/Feature/SampleTest.php`: A basic feature test.
      - `tests/Unit/SampleTest.php`: A basic unit test.

---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testSample()
+    {
+        $this->assertSame(true, true);
+    }
+}
